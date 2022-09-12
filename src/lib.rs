9096e728c8df737dce02b62aaf29c63060718652
Viktor Dudnik <jackfilov@gmail.com>
2022-09-13 01:59:34 +0300

    fix: regex pattern

--- src/lib.rs
+++ src/lib.rs
@@ -22,13 +22,13 @@ pub fn parse_file(path: &Path) -> io::Result<Structure> {
                 directories: vec![],
                 files: vec![],
             };
-            let dir_pattern = Regex::new(r"^\[:(.*)\]$").unwrap();
-            let file_pattern = Regex::new(r"^\[(.*)\]$").unwrap(); // Look aheads aren't supported
+            let dir_pattern = Regex::new(r"\[:(.*)\]").unwrap();
+            let file_pattern = Regex::new(r"\[(.*)\]").unwrap(); // Look aheads aren't supported
             for caps in dir_pattern.captures_iter(&s) {
                 strc.directories.push(caps[1].to_owned())
             }
             for caps in file_pattern.captures_iter(&s) {
-                if caps[1] == "EOF".to_owned() {continue}
+                if caps[1] == "EOF".to_owned() || caps[1].starts_with(':') {continue}
                 let mut file: FileData = FileData {
                     name: String::default(),
                     contents: String::default(),
@@ -36,8 +36,8 @@ pub fn parse_file(path: &Path) -> io::Result<Structure> {
                 file.name = caps[1].to_owned();
                 let start =
                     s.find(format!("[{}]", &file.name).as_str()).unwrap() + 3 + file.name.len();
-                let end = s.split_at(start).1.find("[EOF]\n").unwrap();
-                file.contents = s[start..end].to_owned();
+                let end = s.split_at(start).1.find("[EOF]").unwrap();
+                file.contents = s[start..(start+end)].to_owned();
                 strc.files.push(file)
             }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::path::Path;
+
+use fsn::parse_file;
+
 fn main() {
-    println!("Hello, world!");
+    match parse_file(Path::new("./examples/c.fsn")){
+        Ok(s) => {
+            for dir in s.directories {
+                println!("Dir: {dir}")
+            }
+            for file in s.files {
+                println!("File: {}", file.name);
+                if file.name == "main.c" {
+                    println!("{}", file.contents)
+                }
+            }
+        },
+        Err(e) => println!("err {e}")
+    }
 }
