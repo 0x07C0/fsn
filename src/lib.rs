911cb34444e287e1c71d17952b981b8ee34f42ec
Viktor Dudnik <jackfilov@gmail.com>
2022-11-25 11:31:13 +0200

    refactor: parsing and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-use std::{
-    fs::read_to_string,
-    io::{self},
-    path::Path,
-};
+use std::{error::Error, fs::read_to_string, path::Path};
 
 use regex::Regex;
 
@@ -15,34 +11,57 @@ pub struct Structure {
     pub files: Vec<FileData>,
 }
 
-pub fn parse_file(path: &Path) -> io::Result<Structure> {
-    match read_to_string(path) {
-        Ok(s) => {
-            let mut strc: Structure = Structure {
-                directories: vec![],
-                files: vec![],
-            };
-            let dir_pattern = Regex::new(r"\[:(.*)\]").unwrap();
-            let file_pattern = Regex::new(r"\[(.*)\]").unwrap(); // Look aheads aren't supported
-            for caps in dir_pattern.captures_iter(&s) {
-                strc.directories.push(caps[1].to_owned())
-            }
-            for caps in file_pattern.captures_iter(&s) {
-                if caps[1] == "EOF".to_owned() || caps[1].starts_with(':') {continue}
-                let mut file: FileData = FileData {
-                    name: String::default(),
-                    contents: String::default(),
-                };
-                file.name = caps[1].to_owned();
-                let start =
-                    s.find(format!("[{}]", &file.name).as_str()).unwrap() + 3 + file.name.len();
-                let end = s.split_at(start).1.find("[EOF]").unwrap();
-                file.contents = s[start..(start+end)].to_owned();
-                strc.files.push(file)
-            }
+pub fn parse_file(path: &Path) -> Result<Structure, Box<dyn Error>> {
+    let string = read_to_string(path)?;
 
-            Ok(strc)
+    parse_string(&string)
+}
+
+pub fn parse_string(s: &String) -> Result<Structure, Box<dyn Error>> {
+    let mut structure: Structure = Structure {
+        directories: vec![],
+        files: vec![],
+    };
+
+    let pattern = Regex::new(r"\[(.*)\]")?; // Look-aheads aren't supported
+
+    for caps in pattern.captures_iter(&s) {
+        if caps[1] == "EOF".to_owned() {
+            continue;
         }
-        Err(e) => Err(e),
+        if caps[1].starts_with(':') {
+            structure.directories.push(caps[1][1..].to_owned());
+            continue;
+        }
+        let start = s.find(&format!("[{}]", &caps[1])).unwrap() + 3 + caps[1].len();
+        let end = s.split_at(start).1.find("[EOF]").unwrap_or(start);
+        let file: FileData = FileData {
+            name: caps[1].to_owned(),
+            contents: s[start..(start + end)].to_owned(),
+        };
+        structure.files.push(file)
+    }
+
+    Ok(structure)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::Path;
+    #[test]
+    fn file_parsing() {
+        let structure = parse_file(Path::new("./examples/test.fsn")).unwrap();
+        assert_eq!(structure.directories, vec!["test", "rust/doc"]);
+        assert_eq!(structure.files[0].name, "hello");
+        assert_eq!(structure.files[0].contents, "world\n");
+        assert_eq!(structure.files[1].name, "empty");
+        assert_eq!(structure.files[1].contents, "");
+    }
+    #[test]
+    fn string_parsing() {
+        let string = "[:test]".to_owned();
+        let structure = parse_string(&string).unwrap();
+        assert_eq!(structure.directories, vec!["test"]);
     }
 }
